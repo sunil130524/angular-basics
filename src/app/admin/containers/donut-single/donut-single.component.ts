b5739a82624bdfadb5964bfa11810a9c8c77ce7c
Sunil Kumar C <sunil130524@gmail.com>
2024-12-08 09:29:01 +0530

    Create donut-single and donut-form components

---
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-donut-single',
+  template: `
+    <p>
+      donut-single works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DonutSingleComponent {
+
+}
